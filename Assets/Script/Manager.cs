487f3bff765c23c4eb7fe65702e8b2b81c00f356
ggur_ LabDesktop <gh87913711@gmail.com>
2020-06-10 16:52:35 +0900

    create fileloader, simulate 2 agents

--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public enum FileBrowserType
+[System.Serializable]
+public class Agent
 {
-    File,
-    Directory
+    public float[] posXData;
+    public float[] posYData;
+
+    public Agent()
+    {
+    }
+
 }
-
-
 
 public class Manager : MonoBehaviour
 {
@@ -19,14 +22,35 @@ public class Manager : MonoBehaviour
     private float camYPos;
     public Transform inputFieldTrns;
 
-    public GameObject peopleObj;
+    public GameObject[] peopleObj;
 
-    private string[] dataPath;
+    //private string[] dataPath;
+
+    public InputField dataXPath;
+    public InputField dataYPath;
+
+    private int agentNum=2;
+    public Agent[] agents;
+    GameObject[] agentObjs;
+
+    private bool isLoad=false;
+    public bool isStart = false;
+
+    private int frameNum=0;
+    private int maxFrame = 0;
+    public Slider slider;
+    public Text frameNumTxt;
+
+    private bool first = true;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         camYPos = mainCam.transform.position.y;
+        //초기화
+        agents = new Agent[agentNum];
+        for (int i = 0; i < agents.Length; i++)
+            agents[i] = new Agent();
     }
 
     // Update is called once per frame
@@ -40,5 +64,96 @@ public class Manager : MonoBehaviour
             camYPos = floatInput;
             mainCam.transform.position = new Vector3(mainCam.transform.position.x,camYPos, mainCam.transform.position.z);
         }
+        
+        //로드가 끝났을 때
+        if(isLoad)
+        {
+            frameNumTxt.text = frameNum.ToString();
+           
+            if (first)
+            {
+                agentObjs = new GameObject[agents.Length];
+                //처음일 때  객체 생성
+                for(int i=0;i<agents.Length;i++)
+                {
+                    agentObjs[i] = Instantiate(peopleObj[i], new Vector3(agents[i].posXData[0], 0, agents[i].posYData[0]), Quaternion.identity);
+                }
+                first = false;
+            }
+            else
+            {
+                if(frameNum<agents[0].posXData.Length)
+                {
+                    for(int i=0;i<agentObjs.Length;i++)
+                    {
+                        agentObjs[i].transform.position = new Vector3(agents[i].posXData[frameNum],0, agents[i].posXData[frameNum]);
+                    }
+                    if (isStart)
+                    {
+                        slider.value = frameNum;
+                        frameNum++;
+                    }
+                }
+               
+            }
+        }
+
     }
+
+
+    public void FileLoad()
+    {
+        if(dataXPath!=null&&dataYPath!=null)
+        {
+            //X,Y Pos Data
+            string[] textXValue = System.IO.File.ReadAllLines(dataXPath.text);
+            string[] textYValue = System.IO.File.ReadAllLines(dataYPath.text);
+            //System.IO.StreamReader file =new System.IO.StreamReader(dataXPath.text);
+            maxFrame = textXValue.Length / agentNum;
+            slider.maxValue = maxFrame;
+            for (int i = 0; i < agentNum; i++)
+            {
+                agents[i].posXData = new float[maxFrame];
+                agents[i].posYData = new float[maxFrame];
+            }
+
+            if (textXValue.Length > 0)
+            {
+                for (int i = 0; i < textXValue.Length; i++)
+                {
+                    //Debug.Log(textValue[i]);
+                    float.TryParse(textXValue[i], out agents[i% agentNum].posXData[i/ agentNum]);
+                    float.TryParse(textYValue[i], out agents[i % agentNum].posYData[i / agentNum]);
+                }
+            }
+
+            isLoad = true;
+
+        }
+        else
+        {
+            string errorStr="";
+            if (dataXPath == null)
+                errorStr += "(X Pos Data Path) ";
+            if (dataYPath == null)
+                errorStr += "(Y Pos Data Path) ";
+            Debug.Log(errorStr+"is null");
+        }
+    }
+
+    public void Restart()
+    {
+        frameNum = 0;
+    }
+
+    public void changeStart(bool Active)
+    {
+        this.isStart = Active;
+    }
+
+    public void setSliderValue(Slider slider)
+    {
+        frameNum = (int)slider.value;
+    }
+
 }
